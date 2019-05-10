5d13f7438ed531570b2fcbdbc83821685b2c2edf
Alexandre <alexandredrain@gmail.com>
2019-05-10 16:52:22 +0200

    Version avec du JS pour animation

--- assets/JQuery/index.js
+++ assets/JQuery/index.js
@@ -1,7 +1,20 @@
-$(document).ready(function() {
+// $(document).ready(function() {
+
+// 	$('section figure img').on('click', function() {
+
+// 		$(this).css.transform = 
 
 
+// 	});
 
 
+// });
 
-});
+var r = document.querySelector('section figure img'); //r pour rotation
+function rotate () {
+
+	r.style.transform = 'rotateX(360deg)';
+	r.style.transformOrigin = '0 125px';
+
+}
+document.querySelector('section figure img').addEventListener('click', rotate);
